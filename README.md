8a0a3006e2c9002f9bc195643150a1ebd119dbd1
floalex <flo.liang@my.com>
2016-02-04 23:58:18 +0000

    rock paper scissor game first try

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-This repository is for Launch School Programming Froundations course. 
+#This repository is for Launch School Programming Froundations course. 
 
-Small programs completed in this course:
+##Small command line programs in Ruby completed in this course:
 
-1. Calculator
+###1. Calculator
 
-A command line calculator program in Ruby that does the following:
+A command line calculator program that does the following:
 
--greets user with user's name
--asks for two numbers
--asks for the type of operation to perform: add, subtract, multiply or divide
--displays the result message after the operation
+- greets user with user's name
+- asks for two numbers
+- asks for the type of operation to perform: add, subtract, multiply or divide
+- displays the result message after the operation
+
+###2. Rock Paper Scissor
+
+Rock Paper Scissor game flow:
+
+- Asks the user to make a choice
+- Asks the computer to make a choice
+- Displays the winner
---
+++ rps_game/rock_paper_scissor.rb
@@ -0,0 +1,46 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> #{message}") 
+end
+
+def display_result(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt("Yon won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do 
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+    
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("Please pick the valid options!")
+    end  
+    
+  end
+  
+  computer_choice = VALID_CHOICES.sample
+  
+  Kernel.puts("You chose: #{choice}; computer chose: #{computer_choice}")
+  
+  display_result(choice, computer_choice)
+  
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
